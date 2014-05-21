e56a0420ded205f530512855c1eed5d57b68a235
Will Patera <willpatera@gmail.com>
2014-05-21 17:55:53 +0700

    front-end v0.1.0 complete - next step hook-up to backend and test

--- content/media/js/order-form.js
+++ content/media/js/order-form.js
@@ -1,21 +1,72 @@
 ;(function( $ ) {
 
-  $.fn.copyForm = function(billing, shipping) {
+  $.fn.reveal = function(target) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+
+      $(target).collapse('show');
+    });
+  };
+
+  $.fn.revealNext = function(target) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+
+      targetId = "#" + target + "-" + $(this).attr('id').slice(-1);
+      
+      parent = $(this).parent().parent();
+      $(this).fadeOut("normal", function() {
+        $(parent).remove();
+        $(targetId).collapse('show');
+      }); 
+    });
+    return this;
+  };
+
+  $.fn.toggleAll = function() {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      if ($(this).hasClass('active')) {
+        $('form .collapse').collapse('hide');
+        $("a[id^=next").parent().parent().show();
+        $(this).removeClass('active');
+      } else {      
+        $(this).addClass('active');
+        $("a[id^=next").parent().parent().hide();
+        $('form .collapse').collapse('show');
+      }
+    });
+  };
+
+  $.fn.updateFields = function(target) {
+    $(this).on('input propertychange paste', function(e) {
+      e.preventDefault();
+
+      targetDataId = $(e.target).attr('data-id');
+      targetSelect = target + " .form-control[data-id=" + targetDataId + "]";
+      $(targetSelect).val($(e.target).val());
+    });
+
+    return this;
+  };
+
+  $.fn.copyFields = function(billing, shipping) {
   	$(this).on('click', function(e) {
+      e.preventDefault();
+
   		if($(this).children('input').val() === 'use-other') {
 	  		$(shipping + " .form-control").each(function() {
   				$(this).val("");
   			});
   			$(shipping).collapse('show');
-        $('#submit-collapse').collapse('show');
   		} else {
-        if($(shipping).hasClass("in")) {
-          $(shipping).collapse('hide');
-        }
+        // if($(shipping).hasClass("in")) {
+        //   $(shipping).collapse('hide');
+        // }
 	  		$(billing + " .form-control").each(function() {
   				$(shipping + " .form-control[data-id=" + $(this).attr('data-id') + "]").val($(this).val());
 	  		});
-        $('#submit-collapse').collapse('show');
+        $(shipping).collapse('show');
   		};
   	});
   	return this;
@@ -55,12 +106,25 @@
 
   };
 
-  $.fn.revealShippingIntro = function(target) {
-    $(this).on('click', function(){
-      $(target).collapse('show');
-    });
-    return this;
-  };
 
 
 })( jQuery );
+
+$(document).ready(function() {
+  $('label[data-id=order-type]').revealContact("#order-collapse","input[data-id=order-type]")
+
+
+  $("input[data-action=count]").sumOrder("input[data-id=orderSum]");
+  $('.combobox').combobox();
+  $('[data-toggle=tooltip]').tooltip();
+
+
+  $('a[id^=next]').revealNext('collapse'); //"div[id^=collapse-]"
+  $("label[data-id=shipping-data]").copyFields("#collapse-1", "#shipping-collapse");
+  $('#collapse-0').updateFields('#collapse-1');
+  $('#collapse-1').updateFields('#shipping-collapse');
+  $('label[data-id=payment-data]').reveal('#submit-collapse')
+  $('#toggle-all').toggleAll();
+
+
+});
