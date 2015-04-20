1900b8ded9aa5174a348476de9c0d6645e007f31
Will Patera <willpatera@gmail.com>
2015-04-20 12:06:04 +0700

    incremental work -- working with a fab file setting up helpers

--- fabfile.py
+++ fabfile.py
@@ -4,10 +4,15 @@ import os
 import sys
 import SimpleHTTPServer
 import SocketServer
+import fabric_helpers
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
+
+# setup fabric helper file location
+env.fab_helpers_path = 'fabric_helpers'
+FAB_HELPERS = env.fab_helpers_path
 
 # Remote server configuration
 production = 'root@localhost:22'
@@ -18,26 +23,29 @@ env.cloudfiles_username = 'my_rackspace_username'
 env.cloudfiles_api_key = 'my_rackspace_api_key'
 env.cloudfiles_container = 'my_cloudfiles_container'
 
-
+@task
 def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
-
+@task
 def build():
     local('pelican -s pelicanconf.py')
 
+@task
 def rebuild():
     clean()
     build()
 
+@task
 def regenerate():
     local('pelican -r -s pelicanconf.py')
 
-def serve():
+@task
+def serve(port=8000):
     os.chdir(env.deploy_path)
 
-    PORT = 8000
+    PORT = port
     class AddressReuseTCPServer(SocketServer.TCPServer):
         allow_reuse_address = True
 
@@ -46,13 +54,16 @@ def serve():
     sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
     server.serve_forever()
 
+@task
 def reserve():
     build()
     serve()
 
+@task
 def preview():
     local('pelican -s publishconf.py')
 
+@task
 def cf_upload():
     rebuild()
     local('cd {deploy_path} && '
@@ -60,6 +71,11 @@ def cf_upload():
           '-U {cloudfiles_username} '
           '-K {cloudfiles_api_key} '
           'upload -c {cloudfiles_container} .'.format(**env))
+
+@task
+def new_post(title):
+    fn = fabric_helpers.post_methods.new_post(title)
+    print("File created: " + fname)
 
 @hosts(production)
 def publish():
@@ -71,3 +87,4 @@ def publish():
         delete=True,
         extra_opts='-c',
     )
+
