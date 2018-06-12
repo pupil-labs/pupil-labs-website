95d31888cbb514d76b1a16f6164756c886489401
Will Patera <willpatera@gmail.com>
2018-06-12 18:17:26 +0200

    try npm workaround for travis

--- deploy_before_install.sh
+++ deploy_before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ev
+
+if [[npm -v!= 3* ]]; then npm i -g npm@3; fi
+
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 	echo -e "Host collectivecreature.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 	cat ~/.ssh/config
